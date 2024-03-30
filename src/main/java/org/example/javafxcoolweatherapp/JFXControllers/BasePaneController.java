c31d55b3f3aaf186dff5d8e55a9cef072e6c6542
dmitriy10291029 <dmitriy.lapshin10@gmail.com>
2024-03-31 00:39:25 +0300

    Updated visual interface

--- src/main/java/org/example/javafxcoolweatherapp/JFXControllers/BasePaneController.java
+++ src/main/java/org/example/javafxcoolweatherapp/JFXControllers/BasePaneController.java
@@ -2,22 +2,76 @@ package org.example.javafxcoolweatherapp.JFXControllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import org.example.javafxcoolweatherapp.APIServices.OpenWeather.CurrentForecastAPIService;
 import org.example.javafxcoolweatherapp.APIServices.OpenWeather.GeoAPIService;
-import org.example.javafxcoolweatherapp.APIServices.OpenWeather.HourlyForecastAPIService;
 import org.example.javafxcoolweatherapp.APIServices.OpenWeather.ThreeHourForecastAPIService;
 import org.example.javafxcoolweatherapp.DataObjects.CurrentForecast;
-import org.example.javafxcoolweatherapp.DataObjects.HourlyForecast;
 import org.example.javafxcoolweatherapp.DataObjects.ThreeHourForecast;
 
 import java.io.IOException;
 
-public class BasePaneController {
+public final class BasePaneController {
     @FXML
-    private Label welcomeText;
+    private TextField cityNameField;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private GridPane recentCitiesTable;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private Label day1Date;
+    @FXML
+    private Label day1Temp;
+    @FXML
+    private Label day1Desc;
+    @FXML
+    private Label day2Date;
+    @FXML
+    private Label day2Temp;
+    @FXML
+    private Label day2Desc;
+    @FXML
+    private Label day3Date;
+    @FXML
+    private Label day3Desc;
+    @FXML
+    private Label day3Temp;
+    @FXML
+    private Label day4Date;
+    @FXML
+    private Label day4Temp;
+    @FXML
+    private Label day4Desc;
+    @FXML
+    private Label day5Date;
+    @FXML
+    private Label day5Desc;
+    @FXML
+    private Label day5Temp;
+
+    @FXML
+    private VBox centerPaneVBox;
+    @FXML
+    private Label cityName;
+    @FXML
+    private Label temp;
+    @FXML
+    private Label feelsLike;
+    @FXML
+    private Label weatherDescription;
+    @FXML
+    private Label updateDate;
+
+    @FXML
+    private GridPane detailsTable;
+    @FXML
+    private GridPane hourlyDetails;
+
+    @FXML
+    private void onCitySearchButtonClick() {
         String APIKey = "";
         try {
             GeoAPIService geoAPI = new GeoAPIService(APIKey);
@@ -31,11 +85,8 @@ public class BasePaneController {
             CurrentForecast currentForecast = currentForecastAPIService.getData("Moscow");
             System.out.println(currentForecast);
 
-            welcomeText.setText("Успех");
-
         } catch (IOException e) {
             e.printStackTrace();
-            welcomeText.setText(e.getMessage());
         }
     }
 }
